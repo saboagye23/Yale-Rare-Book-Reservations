a99d7be874602d4a76e0ac7596aa71f5e4a7fccb
saboagye23 <85259473+saboagye23@users.noreply.github.com>
2021-09-25 03:59:45 -0700

    make db host env

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.BR_DB_NAME, process.env.BR_DB_USER, process.env.BR_DB_PW, {
-    host: 'localhost',
+    host: process.env.BR_DB_HOST | 'localhost',
     port: 3306,
     dialect: 'mysql',
     dialectOptions: {
