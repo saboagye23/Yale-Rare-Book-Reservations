89d055e593b4dadc0a143d32ab91e5b5751acb0f
saboagye23 <85259473+saboagye23@users.noreply.github.com>
2021-09-25 04:07:19 -0700

    fix db connection

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,8 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-
 const sequelize = new Sequelize(process.env.BR_DB_NAME, process.env.BR_DB_USER, process.env.BR_DB_PW, {
-    host: process.env.BR_DB_HOST | 'localhost',
+    host: process.env.BR_DB_HOST || 'localhost',
     port: 3306,
     dialect: 'mysql',
     dialectOptions: {
